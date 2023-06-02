3022cb337694d8014cad7ef112b1606d6183b27c
ali <ali@DESKTOP-UNN0KOS>
2023-06-03 01:01:08 +0300

    Proje dosyası ekle.

---
+++ sekil_olustur/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sekil_olustur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            for (int i = 1; i <=7; i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+
+
+            }
+            Console.WriteLine();
+
+            for (int a = 7; a >= 1; a--)
+            {
+                for (int b = 0; b < a; b++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+
+            }
+
+            Console.WriteLine();
+            
+
+            for(int c = 1; c <= 11; c+=2)
+            {
+                for (int f = c; f <= 11; f++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int d = 0; d < c ; d++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+                Console.WriteLine("");
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
